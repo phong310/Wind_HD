b6345190550dfb9710cbf10ccb99590d1b973ba0
Dinh Phong <phamdinhphong12@gmail.com>
2024-06-15 14:45:50 +0700

    update

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -12,8 +12,6 @@ const Images = require('./router/Images')
 dotenv.config()
 const app = express()
 
-app.set('trust proxy', 1);
-
 app.use(cors({
     origin: 'https://wind-storage.netlify.app/',
     credentials: true,
