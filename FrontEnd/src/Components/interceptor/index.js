bfc1d061b87d11631e4df1302e2965576e786b3d
Dinh Phong <phamdinhphong12@gmail.com>
2024-06-16 15:35:43 +0700

    update interceptor fe

--- FrontEnd/src/Components/interceptor/index.js
+++ FrontEnd/src/Components/interceptor/index.js
@@ -1,17 +1,17 @@
 import axios from "axios";
-import jwt_decode from 'jwt-decode'
+import jwt_decode from 'jwt-decode';
 
 const baseURL = import.meta.env.VITE_API_PRODUCTS;
 
-
 const refreshToken = async () => {
     try {
-        const res = await axios.post(`${baseURL}auth/refresh-token`, {} ,{
+        const res = await axios.post(`${baseURL}auth/refresh-token`, {}, {
             withCredentials: true
         });
         return res.data;
     } catch (err) {
         console.log(err);
+        throw err;
     }
 };
 
@@ -19,19 +19,27 @@ export const createAxios = (user, dispatch, stateSuccess) => {
     const newInstance = axios.create({
         baseURL: baseURL,
         withCredentials: true
-    })
+    });
+
     newInstance.interceptors.request.use(
         async (config) => {
             let date = new Date();
             const decodedToken = jwt_decode(user?.accessToken);
             if (decodedToken.exp < date.getTime() / 1000) {
-                const data = await refreshToken();
-                const refreshUser = {
-                    ...user,
-                    accessToken: data.accessToken,
-                };
-                dispatch(stateSuccess(refreshUser));
-                config.headers["token"] = "Bearer " + data.accessToken;
+                try {
+                    const data = await refreshToken();
+                    const refreshUser = {
+                        ...user,
+                        accessToken: data.accessToken,
+                    };
+                    dispatch(stateSuccess(refreshUser));
+                    config.headers["Authorization"] = "Bearer " + data.accessToken;
+                } catch (err) {
+                    console.error('Error refreshing token:', err);
+                    // Handle the error, e.g., redirect to login
+                }
+            } else {
+                config.headers["Authorization"] = "Bearer " + user.accessToken;
             }
             return config;
         },
@@ -39,5 +47,6 @@ export const createAxios = (user, dispatch, stateSuccess) => {
             return Promise.reject(err);
         }
     );
+
     return newInstance;
 };
