71594eb2e7bda810d0bcbfe6dad21f89711afe35
Dinh Phong <phamdinhphong12@gmail.com>
2024-06-16 15:24:12 +0700

    update

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: 'https://wind-storage.netlify.app',
+    origin: 'https://wind-storage.netlify.app/',
     credentials: true,
 }));
 
